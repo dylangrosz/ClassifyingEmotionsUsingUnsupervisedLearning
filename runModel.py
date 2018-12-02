d09191cb197c8ee951a53fcce1bdd23c9c99f55d
dylangrosz <dylangrosz@gmail.com>
2018-12-01 17:32:29 -0800

    plot examples in centers rather than centroid + generalized feature extractor to which we can add more techniques

--- runModel.py
+++ runModel.py
@@ -29,14 +29,36 @@ pics_l = []
 sz = io.imread(img_ex_fn, as_grey=True).flatten().shape[0]
 H, W = io.imread(img_ex_fn, as_grey=True).shape
 
-def norm_and_flatten(img):
-    im_flat = img.flatten()
-    return (im_flat - np.mean(im_flat)) / np.std(im_flat)
+def dmp(image):
+    # takes image and after adding a HOG feature of an
+    # average left/right eye, mouth and nose, we pull out
+    # the HOG and literal window of that area and add to
+    # a flattened feature vector
+    return []
 
-#ADJUST THESE FOR SAVING AND REUSING
+def featureExtract(img, literal=True, norm=True, hog=True, dmp=True):
+    features_p = np.array([])
+    if literal:
+        features_p = img.flatten()
+    if norm:
+        im_flat = img.flatten()
+        features_p = (im_flat - np.mean(im_flat)) / np.std(im_flat)
+    if hog:
+        pixel_per_cell = 8
+        hogFeature, hogImage = feature.hog(img, pixels_per_cell=(pixel_per_cell, pixel_per_cell), visualise=True,
+                                           feature_vector=True)
+        hog_flat = np.array(hogImage).flatten()
+        #        features_p = np.append(features_p, hog_flat)
+        features_p = np.append(features_p, hogFeature)
+    if dmp:
+        dmp_features = dmp(img)
+    return features_p
+
+
+# ADJUST THESE FOR SAVING AND REUSING
 savedYet, toSave = True, True
 cnt = 0
-num_subj = 50
+num_subj = 25
 if not savedYet:
     for subj in listdir(data_fn):
         subj_fn = data_fn + "/" + subj
@@ -51,13 +73,14 @@ if not savedYet:
                     img = io.imread(pic_fn, as_gray=True)
                     H_i, W_i = img.shape
                     if H_i == H and W_i == W:
-                        img = norm_and_flatten(img)
+                        pic_f = featureExtract(img, literal=False)
                         if toSave:
                             with open(feature_fn + "/" + sess + "_" + sess_l[p_i] + "FE.pkl", 'wb') as handle:
-                            #     pkl.dump(img, handle)
+                                #     pkl.dump(img, handle)
                                 np.save(handle, img)
                         pics[subj][sess].append(img)
-                        pics_l.append(img)
+                        print(pic_f.shape)
+                        pics_l.append(pic_f)
 
         print(cnt)
         cnt += 1
@@ -66,13 +89,12 @@ if not savedYet:
 else:
     for f_n in listdir(feature_fn):
         with open(feature_fn + "/" + f_n, 'rb') as handle:
-            #print("go")
-            #img = pkl.load(handle)
-            img = np.load(handle)
+            # img = pkl.load(handle)
+            img = np.load(handle).flatten()
             pics_l.append(img)
 
 print("done")
-pp.pprint(pics)
+pp.pprint(pics_l)
 
 
 def kmeans(examples, K, maxIters):
@@ -185,12 +207,31 @@ def kmeans_fast(features, k, num_iters=100):
 
     return centers, assignments, 0
 
-K = 4
+
+K = 6
 c, a, r_l = kmeans(pics_l, K, 100)
 print(c)
 pp.pprint(a)
+center_assignments = {}
+for i in a:
+    k = a[i]
+    if k in center_assignments:
+        center_assignments[k].append(i)
+    else:
+        center_assignments[k] = [i]
+
+pp.pprint(center_assignments)
+
+
 for i in range(K):
-    plt.subplot(4, 3, i + 1)
-    plt.imshow(c[i].reshape((H, W)), cmap='gray')
+    plt.subplot(4, 5, 3*i + 1)
+#    plt.imshow(c[i][0:(H * W)].reshape((H, W)), cmap='gray')
+    plt.imshow(pics_l[random.choice(center_assignments[i])].reshape((H, W)), cmap='gray')
+    plt.title("%.2f" % i)
+    plt.subplot(4, 3, i + 2)
+    plt.imshow(pics_l[random.choice(center_assignments[i])].reshape((H, W)), cmap='gray')
+    plt.title("%.2f" % i)
+    plt.subplot(4, 3, i + 3)
+    plt.imshow(pics_l[random.choice(center_assignments[i])].reshape((H, W)), cmap='gray')
     plt.title("%.2f" % i)
 plt.show()
